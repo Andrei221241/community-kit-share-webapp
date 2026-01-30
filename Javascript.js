ded686610d6e5b2eb3c18684f79abd5665064e12
issaamtot <issaamtot14@gmail.com>
2026-01-30 12:55:52 +0000

    Run JavaScript after page load

--- Javascript.js
+++ Javascript.js
@@ -1,1 +1,5 @@
-console.log("Javascript.js is loaded");
+console.log("Javascript.js loaded");
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Page fully loaded, JS ready");
+});
